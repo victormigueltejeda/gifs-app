11c3f5a4e6938096efe3e2f680c71692a708ea09
victor miguel <victormigueltejeda@gmail.com>
2023-08-09 10:42:49 -0400

    finish aplication

--- src/app/shared/components/lazy-images/lazy-images.component.ts
+++ src/app/shared/components/lazy-images/lazy-images.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-images',
   templateUrl: './lazy-images.component.html',
   styleUrls: ['./lazy-images.component.css']
 })
-export class LazyImagesComponent {
+export class LazyImagesComponent implements OnInit {
+
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt!: string;
+
+  public hasLoades:boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error("URL property required")
+  }
+
+
+  onLoad() {
+
+    setTimeout(() => {
+      this.hasLoades = true;
+
+    },1000)
+  }
+
+
+
 
 }
